e649a10198de9a91d7bc6b97ebb85712fcc983ce
AnHuy <92672519+danganhuy@users.noreply.github.com>
2025-03-07 10:24:42 +0700

    init

---
+++ src/main/java/c09/workflow_management_api/WorkflowManagementApiApplication.java
@@ -0,0 +1,13 @@
+package c09.workflow_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowManagementApiApplication.class, args);
+    }
+
+}
